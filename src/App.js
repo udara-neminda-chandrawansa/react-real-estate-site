7c891566e0070b914f04c030ba4ac1e5f997f268
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-10-07 15:52:11 +0530

    test-commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Udara was here</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Commit/Publish test
+          Learn React
         </a>
       </header>
     </div>
