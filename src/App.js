e7d5587ed82ef44092f2880d17e628b48a166895
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-10-07 15:44:59 +0530

    test-commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Commit/Publish test
         </a>
       </header>
     </div>
