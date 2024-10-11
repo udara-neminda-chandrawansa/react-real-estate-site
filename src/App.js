66e105882a71da4e42536202f0754fbf797c6e8c
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-10-12 01:46:15 +0530

    featured communities finished

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import './App.css';
 //import Nav from './components/Nav';
 import Banner from './components/Banner';
 import Sidebar from './components/Sidebar';
+import HomeSection1 from './components/HomeSection1';
+import HomeSection2 from './components/HomeSection2';
+import HomeSection3 from './components/HomeSection3';
 
 function App() {
   return (
     <>
-    <div className="App bg-blue-400">
-      {/*<img src={logo} className="" alt="logo" />*/}
-      <Sidebar/>
-      <Banner/>
-    </div>
-    <div className='h-[200px] bg-white'>
-        hi hi hi
+      <div className="App bg-blue-400">
+        {/*<img src={logo} className="" alt="logo" />*/}
+        <Sidebar />
+        <Banner />
       </div>
+      <HomeSection1/>
+      <HomeSection2/>
+      <HomeSection3/>
     </>
   );
 }
